6bfc1f92bdf7eb45963ce34abbb7e34ad3211d44
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-09-13 10:15:20 +0530

    Create Customer model class

---
+++ thogakade_shop_management_system_2/src/main/java/model/Customer.java
@@ -0,0 +1,25 @@
+package model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Customer {
+
+    private String id;
+    private String title;
+    private String name;
+    private Date dob;
+    private double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postalcode;
+
+}
