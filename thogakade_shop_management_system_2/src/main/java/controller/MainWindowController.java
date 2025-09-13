ab305fd9bbc960284df2c7d9274dd74f905e6809
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-09-13 09:21:18 +0530

    Initialize JavaFX project structure

---
+++ thogakade_shop_management_system_2/src/main/java/controller/MainWindowController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainWindowController {
+
+    @FXML
+    private JFXButton custmanagebtn;
+
+    @FXML
+    private JFXButton itemmanagebtn;
+
+    @FXML
+    private JFXButton orderdetailmanagebtn;
+
+    @FXML
+    private JFXButton ordermanagebtn;
+
+    
+
+    public void custmanagebtnOnaction(javafx.event.ActionEvent event) {
+    }
+
+    public void itemmanagebtnOnaction(ActionEvent event) {
+    }
+
+    public void ordermanagebtnOnaction(ActionEvent event) {
+    }
+
+    public void orderdetailmanagebtnOnaction(ActionEvent event) {
+    }
+}
