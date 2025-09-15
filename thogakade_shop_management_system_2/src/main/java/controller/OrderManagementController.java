af57d5d0b7b96e72e18d14b39353642e84dee524
nipunbhanuka12@gmail.com <nipunbhanuka12@gmail.com>
2025-09-15 16:09:58 +0530

    Load orders details to the table

--- thogakade_shop_management_system_2/src/main/java/controller/OrderManagementController.java
+++ thogakade_shop_management_system_2/src/main/java/controller/OrderManagementController.java
@@ -2,13 +2,26 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
+import db.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Order;
 
-public class OrderManagementController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class OrderManagementController implements Initializable {
 
     @FXML
     private JFXButton btndelete;
@@ -32,7 +45,7 @@ public class OrderManagementController {
     private DatePicker dateorderdate;
 
     @FXML
-    private TableView<?> tblordermanagement;
+    private TableView<Order> tblordermanagement;
 
     @FXML
     private JFXTextArea txtcustid;
@@ -48,5 +61,43 @@ public class OrderManagementController {
     }
 
     public void btndeleteOnaction(ActionEvent event) {
+    }
+
+    public void view() {
+
+        ObservableList<Order> orderInfos = FXCollections.observableArrayList();
+
+        try {
+            String SQL = "Select * from orders;";
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                Order order = new Order(
+                        resultSet.getString("OrderID"),
+                        resultSet.getDate("OrderDate"),
+                        resultSet.getString("CustID")
+
+                );
+
+                orderInfos.add(order);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        colorderid.setCellValueFactory(new PropertyValueFactory<>("orderid"));
+        colorderdate.setCellValueFactory(new PropertyValueFactory<>("orderdate"));
+        colcustid.setCellValueFactory(new PropertyValueFactory<>("custid"));
+
+        tblordermanagement.setItems(orderInfos);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        view();
     }
 }
